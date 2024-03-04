24e2a9e7dd786d05bed9b0b34bcf6a76c89903f8
anfo270 <andres44chuy@gmail.com>
2024-03-03 22:15:08 -0700

    correcion manifest y servwork

--- servwork.js
+++ servwork.js
@@ -43,3 +43,7 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+const link = document.createElement('link');
+link.rel = 'manifest';
+link.href = '/manifest.json';
+document.head.appendChild(link);
